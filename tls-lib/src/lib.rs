f01776a3bc00d7027a3561ae05c9e29a1434fa50
momo5502 <mauriceheumann@gmail.com>
2025-11-05 19:59:08 +0100

    Create TLS DLL

--- tls-lib/src/lib.rs
+++ tls-lib/src/lib.rs
@@ -30,3 +30,54 @@ pub extern "system" fn _DllMainCRTStartup(
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
+
+#[repr(C)]
+pub struct ImageTlsDirectory {
+    start_address_of_raw_data: *const u8,
+    end_address_of_raw_data: *const u8,
+    address_of_index: *const u32,
+    address_of_callbacks: *const usize,
+    size_of_zero_fill: u32,
+    characteristics: u32,
+}
+
+unsafe impl Sync for ImageTlsDirectory {}
+
+const TLS_BUFFER_SIZE: usize = 256;
+
+#[unsafe(link_section = ".tls$BBB")]
+#[unsafe(no_mangle)]
+pub static mut _tls_buffer: [u8; TLS_BUFFER_SIZE] = [0; TLS_BUFFER_SIZE];
+
+#[unsafe(no_mangle)]
+pub static mut _tls_index: u32 = 0;
+
+#[unsafe(no_mangle)]
+pub static mut _tls_array: u32 = 0;
+
+#[unsafe(link_section = ".tls$AAA")]
+#[unsafe(no_mangle)]
+pub static mut _tls_start: u8 = 0;
+
+#[unsafe(link_section = ".tls$ZZZ")]
+#[unsafe(no_mangle)]
+pub static mut _tls_end: u8 = 0;
+
+#[unsafe(link_section = ".CRT$XLA")]
+#[unsafe(no_mangle)]
+pub static __xl_a: usize = 0;
+
+#[unsafe(link_section = ".CRT$XLZ")]
+#[unsafe(no_mangle)]
+pub static __xl_z: usize = 0;
+
+#[unsafe(link_section = ".rdata$T")]
+#[unsafe(no_mangle)]
+pub static _tls_used: ImageTlsDirectory = ImageTlsDirectory {
+    start_address_of_raw_data: &raw const _tls_start,
+    end_address_of_raw_data: &raw const _tls_end,
+    address_of_index: &raw const _tls_index,
+    address_of_callbacks: unsafe { (&raw const __xl_a).offset(1) },
+    size_of_zero_fill: 0,
+    characteristics: 0,
+};
